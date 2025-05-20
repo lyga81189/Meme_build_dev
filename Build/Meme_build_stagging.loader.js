58937846bd008fef4d177447e0ae76e23e2a5f2a
ly.dev <ly.nguyenhuong@skybull.studio>
2025-05-20 11:36:07 +0700

    .

--- Build/Meme_build_stagging.loader.js
+++ Build/Meme_build_stagging.loader.js
@@ -54,7 +54,7 @@ function createUnityInstance(canvas, config, onProgress) {
       preserveDrawingBuffer: false,
       powerPreference: 2,
     },
-    wasmFileSize: 81368864,
+    wasmFileSize: 81368928,
     cacheControl: function (url) {
       return (url == Module.dataUrl || url.match(/\.bundle/)) ? "must-revalidate" : "no-store";
     },
